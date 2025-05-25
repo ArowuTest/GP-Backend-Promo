ca2c43105860437b6ff22383bfca6c608f4aeaf5
ArowuTest <fsanus20111@gmail.com>
2025-05-26 00:12:44 +0100

    fixing build errors

--- internal/interface/dto/response/delete_confirmation_response.go
+++
@@ -1,7 +0,0 @@
-package response
-
-// DeleteConfirmationResponse represents a response confirming deletion
-type DeleteConfirmationResponse struct {
-	ID      string `json:"id"`
-	Deleted bool   `json:"deleted"`
-}
